072a5d70fb7c637c0ae6cd9eea5766ad31e415af
mj khodadadi <m_javad_96@yahoo.com>
2022-08-16 01:47:07 -0700

    8/16 0146am {third update}

--- pages/2d.tsx
+++ pages/2d.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import { setMeInfo } from "../app/features/displaySlice";
@@ -32,69 +33,75 @@ const Portfolio2D = () => {
       });
   }, []);
   return (
-    <div className="relative ">
-      <div className="absolute top-0 left-0 w-full min-h-[100vh] bg-[#eee] -z-10">
-        <a
-          href="https://unsplash.com/es/@altumcode?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
-          className="-z-10"
-        >
-          <img
-            src="./2d/banner.jpg"
-            alt=""
-            className="w-full h-[100vh] object-fill -z-10"
-          />
-        </a>
-      </div>
-      <div
-        className={`" fixed top-0 left-0 w-full  py-2 z-50 flex px-3 justify-between items-center transition-colors duration-500 ease-in-out" ${
-          !navStatus ? "bg-transparent" : "bg-black"
-        }`}
-      >
-        <img src="./logo.png" alt="" className="w-12" />
-        <div className="md:flex justify-end items-center pt-1 gap-3 h-fit text-white hidden">
-          <a className="nav-link" href="#about">
-            About
-          </a>
-          <a className="nav-link" href="#portfolio">
-            portfolio
-          </a>
-          <a className="nav-link" href="#services">
-            services
-          </a>
-          <a className="nav-link" href="#resume">
-            resume
-          </a>
-          <a className="nav-link" href="#skills">
-            skills
-          </a>
-          <a className="nav-link" href="#contact">
-            contact
+    <>
+      <Head>
+        <title>2D Portfilio</title>
+        <link sizes="32x32" rel="icon" type="image/png" href="/logo.png" />
+      </Head>
+      <div className="relative ">
+        <div className="absolute top-0 left-0 w-full min-h-[100vh] bg-[#eee] -z-10">
+          <a
+            href="https://unsplash.com/es/@altumcode?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+            className="-z-10"
+          >
+            <img
+              src="./2d/banner.jpg"
+              alt=""
+              className="w-full h-[100vh] object-fill -z-10"
+            />
           </a>
         </div>
+        <div
+          className={`" fixed top-0 left-0 w-full  py-2 z-50 flex px-3 justify-between items-center transition-colors duration-500 ease-in-out" ${
+            !navStatus ? "bg-transparent" : "bg-black"
+          }`}
+        >
+          <img src="./logo.png" alt="" className="w-12" />
+          <div className="md:flex justify-end items-center pt-1 gap-3 h-fit text-white hidden">
+            <a className="nav-link" href="#about">
+              About
+            </a>
+            <a className="nav-link" href="#portfolio">
+              portfolio
+            </a>
+            <a className="nav-link" href="#services">
+              services
+            </a>
+            <a className="nav-link" href="#resume">
+              resume
+            </a>
+            <a className="nav-link" href="#skills">
+              skills
+            </a>
+            <a className="nav-link" href="#contact">
+              contact
+            </a>
+          </div>
+        </div>
+        <Banner />
+        <AboutMe />
+        <PortfolioSec />
+        <Services />
+        <ExpAndEducation />
+        <SkillSets />
+        <Contact2D />
+        <div className="bg-black py-10">
+          <h3 className="text-lg text-gray-500 text-center font-semibold">
+            &copy; Copyright 2022 | Created by Mj
+          </h3>
+          <h5 className="text-md text-gray-500 text-center ">
+            All Rights Reserved
+          </h5>
+        </div>
+        <div className="fixed bottom-3 right-5 ">
+          <Link href="/">
+            <p className="px-4 py-1 text-2xl text-gray-400 font-extrabold animate-pulse ring-1 rounded-lg ring-gray-500 hover:text-cyan-400 hover:ring-cyan-400 cursor-pointer">
+              3D
+            </p>
+          </Link>
+        </div>
       </div>
-      <Banner />
-      <AboutMe />
-      <PortfolioSec />
-      <Services />
-      <ExpAndEducation />
-      <SkillSets />
-      <Contact2D />
-      <div className="bg-black py-10">
-        <h3 className="text-lg text-gray-500 text-center font-semibold">
-          &copy; Copyright 2022 | Created by Mj
-        </h3>
-        <h5 className="text-md text-gray-500 text-center ">
-          All Rights Reserved
-        </h5>
-      </div>
-      <div className="fixed bottom-3 right-5 ">
-        <Link href="/">
-          <p className="px-4 py-1 text-2xl text-gray-400 font-extrabold animate-pulse ring-1 rounded-lg ring-gray-500 hover:text-cyan-400 hover:ring-cyan-400 cursor-pointer">
-            3D
-          </p>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 };
 
